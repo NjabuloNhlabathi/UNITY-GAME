da7b0db27606cbfc687b8cb9292882145c5917ae
Njabulo <njabulonhlabathi223@gmail.com>
2026-06-24 21:50:37 +0200

    Changed the speed in which the balls move

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -5,14 +5,20 @@ public class Obstacle : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     float minSize = 0.5f;
     float maxSize = 2.0f;
+    float minSpeed = 50f;
+    float maxSpeed = 150f;
+
     Rigidbody2D rb;
 
     void Start()
     {
         float randomSize = Random.Range(minSize, maxSize);
         transform.localScale = new Vector3(randomSize, randomSize, 1);
+
         rb = GetComponent<Rigidbody2D>();
-        rb.AddForce(Vector2.right * 100);
+
+        float randomSpeed = Random.Range(minSpeed, maxSpeed);
+        rb.AddForce(Vector2.right * randomSpeed);
     }
 
     // Update is called once per frame
